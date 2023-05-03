17225d42acf49a6e5715106e63ddc1b0b9ac2ca5
sanjeevm0 <sanjeevm@stanfordalumni.org>
2023-05-03 16:28:25 -0700

    update

--- kcclient/log.py
+++ kcclient/log.py
@@ -13,15 +13,16 @@ def mkdir(dir):
 loggers = {}
 logger = None
 
+# if seeing double print messages, set logger.propagate to False as someone may have added a handler to the root logger
 def start_log(filename, level=logging.INFO, prtLogLevel=logging.WARNING, mode='w', loggerName="logcommon-d3ab1c", backupCount=2,
-              fmt=None, prtFmt=None, maxBytes=10*1024*1024, overwrite=False, encoding='utf-8') -> logging.Logger:
+              fmt=None, prtFmt=None, maxBytes=10*1024*1024, overwrite=False, encoding='utf-8', propagate=True) -> logging.Logger:
     global logger
     if loggerName not in loggers or overwrite:
         logger = logging.getLogger(loggerName)
         for handler in logger.handlers: # if overwrite, remove old handlers
             handler.close()
             logger.removeHandler(handler)
-        #logger.propagate = False # optional since not using root logger
+        logger.propagate = propagate # set to False to prevent double printing
         logger.setLevel(min(level, prtLogLevel))
         mkdir(path.dirname(filename))
         if backupCount < 0:
